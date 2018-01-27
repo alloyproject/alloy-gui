5285ab9d262dcdff87c5981a875cdb16e23ef916
AnythingTechPro <anythingtechpro@gmail.com>
2018-01-27 18:22:58 -0500

    updated miniupnpcstrings.h from latest miniupnp

--- src/miniupnpcstrings.h
+++ src/miniupnpcstrings.h
@@ -11,7 +11,15 @@
 #ifndef MINIUPNPCSTRINGS_H_INCLUDED
 #define MINIUPNPCSTRINGS_H_INCLUDED
 
-#define OS_STRING "Windows"
-#define MINIUPNPC_VERSION_STRING "1.9"
+#define OS_STRING "OS/version"
+#define MINIUPNPC_VERSION_STRING "version"
+
+#if 0
+/* according to "UPnP Device Architecture 1.0" */
+#define UPNP_VERSION_STRING "UPnP/1.0"
+#else
+/* according to "UPnP Device Architecture 1.1" */
+#define UPNP_VERSION_STRING "UPnP/1.1"
+#endif
 
 #endif
