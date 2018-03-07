bad572cf69bf2930b63c2d20d6ce6bd6893ed0e5
nighthawk <nighthawk18@protonmail.com>
2018-03-06 16:47:03 -0800

    win build improvements

--- winbuild.cmd
+++ winbuild.cmd
@@ -1,9 +1,12 @@
 @echo off
 copy CMakeLists_Windows.txt CMakeLists.txt
+del /s /q alloy
+echo d | xcopy /y /e \opt\alloy alloy
 mkdir build
 del /s /q build
 cd build
-"C:\Program Files\CMake\bin\cmake.exe" -G  "Visual Studio 14 2015 Win64"  ..
+
+cmake.exe -G  "Visual Studio 14 2015 Win64"  ..
 
 msbuild AlloyWallet.sln /p:Configuration=Release
 
